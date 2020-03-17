b86f26e3351c73e655a20fd88c1a1f6efd3e7721
Stas <staslimon@gmail.com>
2020-03-17 16:34:31 +0200

    personal page logic added

---
+++ system/Pages/UserPersonalController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TestSystem\Pages;
+
+use TestSystem\AbstractController;
+use TestSystem\Auth;
+
+class UserPersonalController extends AbstractController
+{
+    public function init()
+    {
+        if (!Auth::getInstance()->ifLogged()) {
+            header('location: ' . SITE_URL . CURRENT_LANG . '/login', null, 301);
+            die();
+        }
+
+        $user = \DB::table('users')->find(Auth::getInstance()->getUserId());
+
+        return $this->view->render('personal', ['user' => $user]);
+    }
+}
---
+++ views/personal.blade.php
@@ -0,0 +1,16 @@
+@extends('index')
+
+@section('content')
+    <div class="row">
+        <div class="card col-md-6 mx-auto">
+            @if ($user->image)
+                <img src="/public/storage/users/{{ $user->id }}/{{ $user->image }}" class="card-img-top" alt="...">
+            @endif
+            <div class="card-body">
+                <h5 class="card-title">{{ $user->name }} {{ $user->secondname }}</h5>
+                <p class="card-text">Email: {{ $user->email }}</p>
+                <a href="{{ $SITE_URL }}{{ CURRENT_LANG }}/logout" class="btn btn-primary">{{ _('Выход') }}</a>
+            </div>
+        </div>
+    </div>
+@endsection
